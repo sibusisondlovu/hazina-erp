f9db1e1cf498671af8b18c9598ab60d62c1f9704
Sibusiso Ndlovu <sibusiso@jaspa.co.za>
2021-06-13 10:32:38 +0200

    database helper class methods

--- app/src/main/java/co/za/jaspa/hazinaerp/DatabaseHelper.java
+++ app/src/main/java/co/za/jaspa/hazinaerp/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package co.za.jaspa.hazinaerp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class DatabaseHelper  extends SQLiteOpenHelper {
 
@@ -27,5 +29,46 @@ public class DatabaseHelper  extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS customers");
         onCreate(db);
+    }
+
+    private void createCustomer(String customerName, String contactNumber, String emailAddress) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put("fullNames",customerName);
+        cv.put("contactNumber",contactNumber);
+        cv.put("emailAddress",emailAddress);
+
+        long result = db.insert("customers",null,cv);
+
+        if (result == -1) {
+            Toast.makeText(context, "Failed to create new customer", Toast.LENGTH_SHORT).show();
+        }else{
+            //create customer order
+            Toast.makeText(context, "Customer created successfully,... creating customer order...", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void createOrder(String style, String fittingDate, String collectionDate, double deposit, double totalCost, int customerId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put("customerId",customerId);
+        cv.put("style",style);
+        cv.put("fittingDate",fittingDate);
+        cv.put("collectionDate",collectionDate);
+        cv.put("totalCost",totalCost);
+        cv.put("depositPaid",deposit);
+
+        long result = db.insert("orders",null,cv);
+
+        if (result == -1) {
+            Toast.makeText(context, "Failed to create new order", Toast.LENGTH_SHORT).show();
+        }else{
+            //create customer order
+            Toast.makeText(context, "Order created successfully,... adding customer measurements to order...", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/co/za/jaspa/hazinaerp/OrdersActivity.java
+++ app/src/main/java/co/za/jaspa/hazinaerp/OrdersActivity.java
@@ -1,19 +1,16 @@
+
+
 package co.za.jaspa.hazinaerp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-public class NewOrderActivity extends AppCompatActivity {
-
+public class OrdersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_order);
+        setContentView(R.layout.activity_orders);
     }
-
 }
